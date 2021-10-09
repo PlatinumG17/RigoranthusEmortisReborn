1bd566e039fd9fceda301027e5cb0f41dec30488
Matt <Platinumg17@gmail.com>
2021-10-09 06:11:04 -0400

    Added Bone Bow
    Signed-off-by: Matt <Platinumg17@gmail.com>

--- src/main/java/com/platinumg17/rigoranthusemortisreborn/items/weapons/BoneBow.java
+++ src/main/java/com/platinumg17/rigoranthusemortisreborn/items/weapons/BoneBow.java
@@ -1,6 +1,7 @@
 package com.platinumg17.rigoranthusemortisreborn.items.weapons;
 
 import com.platinumg17.rigoranthusemortisreborn.RigoranthusEmortisReborn;
+import com.platinumg17.rigoranthusemortisreborn.config.Config;
 import net.minecraft.item.BowItem;
 import net.minecraft.stats.Stats;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -21,14 +22,19 @@ public class BoneBow extends BowItem {
     }
 
     @Override
-    public void releaseUsing(ItemStack p_77615_1_, World p_77615_2_, LivingEntity p_77615_3_, int p_77615_4_) {
-        if (p_77615_3_ instanceof PlayerEntity) {
-            PlayerEntity playerentity = (PlayerEntity) p_77615_3_;
-            boolean flag = playerentity.abilities.instabuild || EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, p_77615_1_) > 0;
-            ItemStack itemstack = playerentity.getProjectile(p_77615_1_);
+    public int getDefaultProjectileRange() {
+        return Config.bone_bow_projectile_range.get();
+    }
 
-            int i = this.getUseDuration(p_77615_1_) - p_77615_4_;
-            i = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(p_77615_1_, p_77615_2_, playerentity, i, !itemstack.isEmpty() || flag);
+    @Override
+    public void releaseUsing(ItemStack stack, World worldIn, LivingEntity entityLiving, int timeLeft) {
+        if (entityLiving instanceof PlayerEntity) {
+            PlayerEntity playerentity = (PlayerEntity) entityLiving;
+            boolean flag = playerentity.abilities.instabuild || EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, stack) > 0;
+            ItemStack itemstack = playerentity.getProjectile(stack);
+
+            int i = this.getUseDuration(stack) - timeLeft;
+            i = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(stack, worldIn, playerentity, i, !itemstack.isEmpty() || flag);
             if (i < 0) return;
 
             if (!itemstack.isEmpty() || flag) {
@@ -38,41 +44,41 @@ public class BoneBow extends BowItem {
 
                 float f = getPowerForTime(i);
                 if (!((double) f < 0.1D)) {
-                    boolean flag1 = playerentity.abilities.instabuild || (itemstack.getItem() instanceof ArrowItem && ((ArrowItem) itemstack.getItem()).isInfinite(itemstack, p_77615_1_, playerentity));
-                    if (!p_77615_2_.isClientSide) {
+                    boolean flag1 = playerentity.abilities.instabuild || (itemstack.getItem() instanceof ArrowItem && ((ArrowItem) itemstack.getItem()).isInfinite(itemstack, stack, playerentity));
+                    if (!worldIn.isClientSide) {
                         ArrowItem arrowitem = (ArrowItem) (itemstack.getItem() instanceof ArrowItem ? itemstack.getItem() : Items.ARROW);
-                        AbstractArrowEntity abstractarrowentity = arrowitem.createArrow(p_77615_2_, itemstack, playerentity);
+                        AbstractArrowEntity abstractarrowentity = arrowitem.createArrow(worldIn, itemstack, playerentity);
                         abstractarrowentity = customArrow(abstractarrowentity);
                         abstractarrowentity.shootFromRotation(playerentity, playerentity.xRot, playerentity.yRot, 0.0F, f * 3.0F, 1.0F);
                         if (f == 1.0F) {
                             abstractarrowentity.setCritArrow(true);
                         }
 
-                        int j = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.POWER_ARROWS, p_77615_1_);
+                        int j = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.POWER_ARROWS, stack);
                         if (j > 0) {
-                            abstractarrowentity.setBaseDamage(abstractarrowentity.getBaseDamage() + (double) j * 0.5D + 0.5D);
+                            abstractarrowentity.setBaseDamage(abstractarrowentity.getBaseDamage() + (double) j * 0.6D + 0.6D);
                         }
 
-                        int k = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.PUNCH_ARROWS, p_77615_1_);
+                        int k = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.PUNCH_ARROWS, stack);
                         if (k > 0) {
                             abstractarrowentity.setKnockback(k);
                         }
 
-                        if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FLAMING_ARROWS, p_77615_1_) > 0) {
-                            abstractarrowentity.setSecondsOnFire(100);
+                        if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FLAMING_ARROWS, stack) > 0) {
+                            abstractarrowentity.setSecondsOnFire(110);
                         }
 
-                        p_77615_1_.hurtAndBreak(1, playerentity, (p_220009_1_) -> {
-                            p_220009_1_.broadcastBreakEvent(playerentity.getUsedItemHand());
+                        stack.hurtAndBreak(1, playerentity, (player) -> {
+                            player.broadcastBreakEvent(playerentity.getUsedItemHand());
                         });
                         if (flag1 || playerentity.abilities.instabuild && (itemstack.getItem() == Items.SPECTRAL_ARROW || itemstack.getItem() == Items.TIPPED_ARROW)) {
                             abstractarrowentity.pickup = AbstractArrowEntity.PickupStatus.CREATIVE_ONLY;
                         }
                         abstractarrowentity.getPersistentData().putBoolean(RigoranthusEmortisReborn.MOD_ID + "bone_bow", true);
-                        p_77615_2_.addFreshEntity(abstractarrowentity);
+                        worldIn.addFreshEntity(abstractarrowentity);
                     }
 
-                    p_77615_2_.playSound((PlayerEntity) null, playerentity.getX(), playerentity.getY(), playerentity.getZ(), SoundEvents.ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (random.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+                    worldIn.playSound((PlayerEntity) null, playerentity.getX(), playerentity.getY(), playerentity.getZ(), SoundEvents.ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (random.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
                     if (!flag1 && !playerentity.abilities.instabuild) {
                         itemstack.shrink(1);
                         if (itemstack.isEmpty()) {

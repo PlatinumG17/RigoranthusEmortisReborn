0138f637f99dc19844d9534bea596c147873fd85
Matt <Platinumg17@gmail.com>
2021-10-12 13:43:55 -0400

    Custom Recipe attempts
    Signed-off-by: Matt <Platinumg17@gmail.com>

--- src/main/java/com/platinumg17/rigoranthusemortisreborn/core/registry/effects/RigoranthusEffectRegistry.java
+++ src/main/java/com/platinumg17/rigoranthusemortisreborn/core/registry/effects/RigoranthusEffectRegistry.java
@@ -3,8 +3,13 @@ package com.platinumg17.rigoranthusemortisreborn.core.registry.effects;
 import com.platinumg17.rigoranthusemortisreborn.RigoranthusEmortisReborn;
 import com.platinumg17.rigoranthusemortisreborn.core.init.ItemInit;
 import com.platinumg17.rigoranthusemortisreborn.items.armor.bonuses.*;
-import net.minecraft.item.Item;
-import net.minecraftforge.eventbus.api.IEventBus;
+//import com.platinumg17.rigoranthusemortisreborn.util.PotionRecipeUtils;
+//import net.minecraft.item.Item;
+//import net.minecraft.util.ResourceLocation;
+//import net.minecraft.util.registry.Registry;
+import com.platinumg17.rigoranthusemortisreborn.util.PactBrewing;
+import net.minecraftforge.common.brewing.BrewingRecipe;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -13,10 +18,10 @@ import net.minecraft.potion.*;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = RigoranthusEmortisReborn.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RigoranthusEffectRegistry {
@@ -65,10 +70,15 @@ public class RigoranthusEffectRegistry {
     public static ItemStack createPotion(Potion potion){
         return  PotionUtils.setPotion(new ItemStack(Items.POTION), potion);
     }
+//    public static ItemStack createPotionRecipe(ItemInit item){
+//        return ItemStack(ItemInit.PACT_OF_SERVITUDE), item);
+//    }
 
     public static void onInitItems(){
-        BrewingRecipeRegistry.addRecipe(Ingredient.of(createPotion(Potions.STRENGTH)), Ingredient.of(ItemInit.DWELLER_FLESH.get()), createPotion(NECROTIZING_FASCIITIS_POTION));
-        BrewingRecipeRegistry.addRecipe(new EmortisBrewing(Ingredient.of(createPotion(NECROTIZING_FASCIITIS_II_POTION)), Ingredient.of(Items.REDSTONE), createPotion(NECROTIZING_FASCIITIS_II_POTION)));
+        //  createPotionRecipe(ItemInit.PACT_OF_SERVITUDE.get())));
+        BrewingRecipeRegistry.addRecipe(Ingredient.of(createPotion(Potions.AWKWARD)), Ingredient.of(ItemInit.DWELLER_FLESH.get()), createPotion(NECROTIZING_FASCIITIS_POTION));
+        BrewingRecipeRegistry.addRecipe(new EmortisBrewing(Ingredient.of(createPotion(NECROTIZING_FASCIITIS_POTION)), Ingredient.of(Items.REDSTONE), createPotion(NECROTIZING_FASCIITIS_II_POTION)));
+//        BrewingRecipeRegistry.addRecipe(new PactBrewing(Ingredient.of(Items.PAPER), Ingredient.of(ItemInit.BOTTLE_OF_ICHOR.get()), ItemStack.of(ItemInit.PACT_OF_SERVITUDE.get())));
     }
 
 }

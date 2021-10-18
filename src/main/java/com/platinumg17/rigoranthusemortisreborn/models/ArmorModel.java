955a93afb8edb7c3d4e0a5c696e3288040b20b83
Matt <Platinumg17@gmail.com>
2021-10-18 10:44:47 -0400

    Cleanup code
    Signed-off-by: Matt <Platinumg17@gmail.com>

--- src/main/java/com/platinumg17/rigoranthusemortisreborn/models/ArmorModel.java
+++ src/main/java/com/platinumg17/rigoranthusemortisreborn/models/ArmorModel.java
@@ -8,35 +8,22 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 
 public class ArmorModel <T extends LivingEntity> extends BipedModel<T> {
-
     protected final EquipmentSlotType slotType;
-
     protected ModelRenderer Body;
-//    protected ModelRenderer RightArm;
-//    protected ModelRenderer LeftArm;
 
     public ArmorModel(float modelSize, EquipmentSlotType slotType) {
         super(modelSize, 0, 32, 32);
         this.slotType = slotType;
     }
 
-   @Override
+    @Override
     public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
         matrixStack.pushPose();
 
         if (this.slotType == EquipmentSlotType.CHEST) {
             Body.copyFrom(this.Body);
             Body.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-//            RightArm.copyFrom(this.RightArm);
-//            RightArm.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-//            LeftArm.copyFrom(this.LeftArm);
-//            LeftArm.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
             matrixStack.popPose();
         }
     }
-    //public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-    //    modelRenderer.xRot = x;
-    //    modelRenderer.yRot = y;
-    //    modelRenderer.zRot = z;
-   // }
 }

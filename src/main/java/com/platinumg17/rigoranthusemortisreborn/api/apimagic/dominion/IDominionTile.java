e3686eb02750f7c33d9e837fab699025d9fd5fa2
Matt <Platinumg17@gmail.com>
2021-11-21 11:59:41 -0500

    Early Magic Edit 1
    Signed-off-by: Matt <Platinumg17@gmail.com>

--- src/main/java/com/platinumg17/rigoranthusemortisreborn/api/apimagic/dominion/IDominionTile.java
+++ src/main/java/com/platinumg17/rigoranthusemortisreborn/api/apimagic/dominion/IDominionTile.java
@@ -1,23 +1,15 @@
 package com.platinumg17.rigoranthusemortisreborn.api.apimagic.dominion;
 
 /**
- * Interface for a generic tile that holds mana.
+ * Interface for a generic tile that holds dominion.
  */
 public interface IDominionTile {
-
     int getTransferRate();
-
     boolean canAcceptDominion();
-
     int getCurrentDominion();
-
     int getMaxDominion();
-
     void setMaxDominion(int max);
-
-    int setDominion(final int mana);
-
-    int addDominion(final int manaToAdd);
-
-    int removeDominion(final int manaToRemove);
+    int setDominion(final int dominion);
+    int addDominion(final int dominionToAdd);
+    int removeDominion(final int dominionToRemove);
 }

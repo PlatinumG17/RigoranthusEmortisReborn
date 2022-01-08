f157b397809f2a45f89d40dfdfd2394702a70e82
Matt <Platinumg17@gmail.com>
2022-01-07 23:00:03 -0500

    Major Improvements to Canis / Skills/ Inventories / etc.
    Signed-off-by: Matt <Platinumg17@gmail.com>

--- src/main/java/com/platinumg17/rigoranthusemortisreborn/canis/common/items/CanisShearsItem.java
+++ src/main/java/com/platinumg17/rigoranthusemortisreborn/canis/common/items/CanisShearsItem.java
@@ -1,11 +1,14 @@
 package com.platinumg17.rigoranthusemortisreborn.canis.common.items;
 
 import com.platinumg17.rigoranthusemortisreborn.canis.common.lib.EmortisConstants;
+import com.platinumg17.rigoranthusemortisreborn.core.init.ItemInit;
+import com.platinumg17.rigoranthusemortisreborn.magica.common.util.PortUtil;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import com.platinumg17.rigoranthusemortisreborn.api.apicanis.feature.DataKey;
 import com.platinumg17.rigoranthusemortisreborn.api.apicanis.entity.AbstractCanisEntity;
@@ -32,23 +35,23 @@ public class CanisShearsItem extends Item implements ICanisItem {
                 }
                 if (!worldIn.isClientSide) {
                     int cooldownLeft = canisIn.getDataOrDefault(COOLDOWN, canisIn.tickCount) - canisIn.tickCount;
-
                     if (cooldownLeft <= 0) {
                         worldIn.broadcastEntityEvent(canisIn, EmortisConstants.EntityState.CANIS_SMOKE);
-                        canisIn.untame();
+                        if (playerIn.getOffhandItem() == ItemInit.PACT_OF_SERVITUDE.get().getDefaultInstance()) {
+                            canisIn.untame();
+                        }
                     }
                 }
                 return ActionResultType.SUCCESS;
             }
-
             if (!worldIn.isClientSide) {
                 for (AccoutrementInstance inst : accouterments) {
                     ItemStack returnItem = inst.getReturnItem();
                     canisIn.spawnAtLocation(returnItem, 1);
                 }
                 canisIn.removeAccouterments();
-                canisIn.setData(COOLDOWN, canisIn.tickCount + 40);
-
+                canisIn.setData(COOLDOWN, canisIn.tickCount + 20);
+                PortUtil.sendMessage(playerIn, new TranslationTextComponent("entity.rigoranthusemortisreborn.canis.beware"));
                 return ActionResultType.SUCCESS;
             }
             return ActionResultType.SUCCESS;
